5c3c15d99515d102d27128247e8a2e922fcc868c
rifatahmed787 <mdrifatahmed787@gmail.com>
2025-03-16 17:18:41 +0600

    dist

---
+++ dist/icon/IconSeat.tsx
@@ -0,0 +1,29 @@
+export const IconSeat = (props: any) => (
+  <svg
+    id="Icons"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 32 32"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style type="text/css">
+      {
+        "\n\t.st0{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}\n"
+      }
+    </style>
+    <path
+      className="st0"
+      d="M9,29H5c-1.1,0-2-0.9-2-2V17c0-1.7,1.3-3,3-3h0c1.7,0,3,1.3,3,3V29z"
+    />
+    <path
+      className="st0"
+      d="M27,29h-4V17c0-1.7,1.3-3,3-3h0c1.7,0,3,1.3,3,3v10C29,28.1,28.1,29,27,29z"
+    />
+    <rect x={9} y={19} className="st0" width={14} height={10} />
+    <path
+      className="st0"
+      d="M6,14V7.8C6,5.7,7.7,4,9.8,4h12.3C24.3,4,26,5.7,26,7.8V14"
+    />
+  </svg>
+);
